06653667e0b3453932f7355f63c330f83dcbc27a
Zynyx <57494687+ZynyxTheFirst@users.noreply.github.com>
2021-04-16 14:42:08 +0200

    Fixed link between things and stuff :D

--- Databas/DatbasCore/Program.cs
+++ Databas/DatbasCore/Program.cs
@@ -21,28 +21,26 @@ class Program
             switch (input)
             {
                 case "save":
-                    Save();
                     break;
                 case "load":
-                    Load();
                     break;
                 case "quit":
                     return;
                 default:
-                    var p = SqliteDataAccess.LoadPerson(input);
-                    Console.WriteLine($"id: {p.id}, name: {p.first_name} {p.last_name}");
                     break;
             }
         }
     }
-
+    //Fix at some point
+    /*
     void Load()
     {
-        var list = SqliteDataAccess.LoadPeople();
-        foreach (PersonModel p in list)
-            Console.WriteLine($"id: {p.id}, name: {p.first_name} {p.last_name}");
+        var list = SqliteDataAccess.LoadElever();
+        foreach (EleverModel p in list)
+            Console.WriteLine($"id: {p.Elev_Personnummer}, name: {p.Namn} {p.Adress}");
     }
 
+    //skriv om
     void Save()
     {
         Console.WriteLine("First name or quit: \n>>");
@@ -53,51 +51,7 @@ class Program
         Console.WriteLine("Last name: \n>>");
         string lastName = Console.ReadLine();
 
-        SqliteDataAccess.SavePerson(new PersonModel() { first_name = name, last_name = lastName });
+        SqliteDataAccess.AddElev(new EleverModel() { Namn = name, Adress = lastName });
     }
-
-
+    */
 }
-
-class SqliteDataAccess
-{
-    public static List<PersonModel> LoadPeople()
-    {
-        using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
-        {
-            var output = cnn.Query<PersonModel>("SELECT * FROM people", new DynamicParameters());
-            return output.ToList();
-        }
-    }
-
-    public static PersonModel LoadPerson(string firstName)
-    {
-        using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
-        {
-            var output = cnn.Query<PersonModel>("SELECT * FROM people WHERE first_name='" + firstName + "'", new DynamicParameters());
-            return output.ToList()[0];
-        }
-    }
-
-    public static void SavePerson(PersonModel person)
-    {
-        using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
-        {
-            cnn.Execute("INSERT INTO people (first_name, last_name) VALUES (@FirstName, @LastName)", person);
-        }
-    }
-
-    private static string LoadConnectionString(string id = "Default")
-    {
-        //gå in i App.config och ge vår ConnectionString till oss!
-        return ConfigurationManager.ConnectionStrings[id].ConnectionString;
-    }
-}
-
-public class PersonModel
-{
-    public int id { get; set; }
-    public string first_name { get; set; }
-    public string last_name { get; set; }
-}
-
